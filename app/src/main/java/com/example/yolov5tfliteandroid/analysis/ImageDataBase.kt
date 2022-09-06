a445ff43052c0cc96d8a823e2c07f4399f8f912e
luzhiqian00 <941472870@qq.com>
2022-09-06 20:10:14 +0800

    database

---
+++ app/src/main/java/com/example/yolov5tfliteandroid/analysis/ImageDataBase.kt
@@ -0,0 +1,33 @@
+package com.example.yolov5tfliteandroid.analysis
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+class ImageDataBase(val context: Context,name:String,version:Int):
+            SQLiteOpenHelper(context,name,null,version){
+
+    private val createImage="create table ImageStore("+
+            "id integer primary key autoincrement,"+
+            "labelName String,"+
+            "labelScore Float,"+
+            "confidence Float)"
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(createImage)
+        Toast.makeText(context,"Create succeeded",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
+    fun put_together(labelName: String,labelSore:Float,confidence:Float):ContentValues{
+        val values=ContentValues().apply {
+            put("labelName",labelName)
+            put("labelScore",labelSore)
+            put("confidence",confidence)
+        }
+        return values
+    } }
