5479c966271b5d4cafbf1c65227bf22fd0fe8883
luzhiqian00 <941472870@qq.com>
2022-09-18 22:45:36 +0800

    detect

--- app/src/main/java/com/example/yolov5tfliteandroid/ui/history/ItemActivity.java
+++ app/src/main/java/com/example/yolov5tfliteandroid/ui/history/ItemActivity.java
@@ -1,19 +1,26 @@
 package com.example.yolov5tfliteandroid.ui.history;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yolov5tfliteandroid.R;
+import com.example.yolov5tfliteandroid.YAApplication;
+import com.example.yolov5tfliteandroid.com.example.yolov5tfliteandroid.view.EMImageView;
+
+import java.io.File;
 
 public class ItemActivity extends AppCompatActivity {
-    private int id;
-    private ImageView imageView;
+    private Integer id;
+
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,16 @@ public class ItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item);
         Intent intent = getIntent();
         id=intent.getIntExtra("position", 0);
-        imageView=findViewById(R.id.image_item);
+        EMImageView  imageView=findViewById(R.id.image_item);
         textView=findViewById(R.id.text_item);
         textView.setText("Item:"+id);
+        File file=new File(YAApplication.context.getFilesDir().toString() + "/image" +id.toString() + ".png");
+
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getPath()).copy(Bitmap.Config.ARGB_8888, true);
+//        Bitmap emptyCropSizeBitmap=Bitmap.createBitmap(1080,1440,Bitmap.Config.ARGB_8888);
+        Canvas canvas =new Canvas(bitmap);
+        imageView.draw(canvas,id);
+        imageView.setImageBitmap(bitmap);
+
     }
 }
---
+++ app/src/main/java/com/example/yolov5tfliteandroid/view/EMImageView.kt
@@ -0,0 +1,47 @@
+package com.example.yolov5tfliteandroid.com.example.yolov5tfliteandroid.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import com.example.yolov5tfliteandroid.YAApplication
+import com.example.yolov5tfliteandroid.analysis.AppDataBase
+import com.example.yolov5tfliteandroid.analysis.ImageDataBase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class EMImageView (context:Context,attrs:AttributeSet):
+    androidx.appcompat.widget.AppCompatImageView(context,attrs){
+
+     fun draw(canvas: Canvas?,id:Integer) {
+        super.draw(canvas)
+        val paintRect= Paint()
+        paintRect.color=Color.rgb(108,10,13)
+        paintRect.strokeWidth=1f
+        paintRect.style=Paint.Style.STROKE
+
+        // 字体画笔
+        val textPain = Paint()
+        textPain.textSize = 30f
+        textPain.color = Color.RED
+        textPain.style = Paint.Style.FILL
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val userDao= AppDataBase.getDatabase(YAApplication.context).imageDataBaseDao();//如何使用ROOM
+            val imageList:List<ImageDataBase> =userDao.loadImageDabaBaseImageNumber("image${id}.png");
+            for(data in imageList){
+                canvas?.apply {
+                    drawRect(
+                        (640-data.bottom*0.6).toFloat(), (data.left).toFloat(),
+                        (640-data.top*0.6).toFloat(), (data.right).toFloat(),paintRect)
+                    drawText(data.label+":"+String.format("%.2f",data.confidence),
+                        (640-data.bottom*0.6).toFloat(),data.left,textPain)
+                }
+            }
+        }
+    }
+
+
+}
