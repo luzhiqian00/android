d8de80ce04703aba9cf82688232d6d2d52824c49
Tianhenzhi <747012804@qq.com>
2022-09-20 22:34:20 +0800

    for pull

--- app/src/main/java/com/example/yolov5tfliteandroid/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/yolov5tfliteandroid/ui/login/LoginActivity.java
@@ -2,23 +2,29 @@ package com.example.yolov5tfliteandroid.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.yolov5tfliteandroid.Bottom;
 import com.example.yolov5tfliteandroid.R;
+import com.example.yolov5tfliteandroid.web.network;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText userName;
     private EditText passWord;
     private Button loginButton;
     private Button signUpButton;
+    //String test=network.Server;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +49,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 String strUserName = userName.getText().toString();
                 String strPassWord = passWord.getText().toString();
                 //TODO 账号密码处理
-                if (strUserName.equals("123456") && strPassWord.equals("123456")) {
+                final Map<String, String> map = new HashMap<>();
+                map.put("name", strUserName);
+                map.put("pwd", strPassWord);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Looper.prepare();
+                        JSONObject ansJson = network.mapPOST(network.Server + "connect.php", map);
+                        try {
+                            if (ansJson.getInt("res") == 1) {//登录成功
+                                Toast.makeText(getApplicationContext(), "登录成功",
+                                        Toast.LENGTH_LONG).show();
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "登录失败：" + ansJson.getInt("res") + "，可能的原因：" + ansJson.getString("reason"),
+                                        Toast.LENGTH_LONG).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Looper.loop();
+                    }
+                }).start();
+/*                if (strUserName.equals("123456") && strPassWord.equals("123456")) {
                     Toast.makeText(LoginActivity.this, "登录成功！", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(LoginActivity.this, Bottom.class);
                     startActivity(intent);
                 } else {
                     Toast.makeText(LoginActivity.this, "请输入正确的用户名或密码！", Toast.LENGTH_SHORT).show();
-                }
+                }*/
                 break;
             case R.id.SignUpButton:
                 // 跳转到注册界面
