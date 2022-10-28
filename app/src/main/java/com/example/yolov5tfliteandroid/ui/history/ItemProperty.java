bde69a1283d0c41ec7d59c570cfeb0e57f206c92
Tianhenzhi <747012804@qq.com>
2022-10-28 11:09:12 +0800

    时间接口

--- app/src/main/java/com/example/yolov5tfliteandroid/ui/history/ItemProperty.java
+++ app/src/main/java/com/example/yolov5tfliteandroid/ui/history/ItemProperty.java
@@ -1,7 +1,8 @@
 package com.example.yolov5tfliteandroid.ui.history;
 
 public class ItemProperty {
-    private String title; //item标题
+    private String time1; //item标题
+    private String time2;
     private boolean isSelect; //是否选中
     private String imagePath;   // 图片本地地址
     private int imageId;   //图片的id，也是存储的数据库中的id
@@ -10,21 +11,25 @@ public class ItemProperty {
 
     public void setImageId(int imageId) {this.imageId = imageId;}
 
-    public String getTitle() {
-        return title;
+    public String getTime1() {
+        return time1;
     }
 
     public boolean isSelect() {
         return isSelect;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setTime1(String time) {
+        this.time1 = time;
     }
+
+    public void setTime2(String time){this.time2=time;}
 
     public void setSelect(boolean select) {
         isSelect = select;
     }
+
+    public String getTime2(){return time2;}
 
     public String getImagePath() {
         return imagePath;
@@ -36,4 +41,6 @@ public class ItemProperty {
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
+
+
 }
