d5b3e870365a35c65b02a58932ba60e39612aff5
Tianhenzhi <747012804@qq.com>
2023-03-31 20:17:14 +0800

    修改输入模型大小|历史记录可查看

--- app/src/main/java/com/example/yolov5tfliteandroid/detector/Yolov5TFLiteDetector.java
+++ app/src/main/java/com/example/yolov5tfliteandroid/detector/Yolov5TFLiteDetector.java
@@ -45,13 +45,14 @@ import java.util.PriorityQueue;
 
 public class Yolov5TFLiteDetector {
 
-    private final Size INPNUT_SIZE = new Size(640, 640);
-    private final int[] OUTPUT_SIZE = new int[]{1, 25200, 9};
+    private final Size INPNUT_SIZE = new Size(320, 320);
+    private final int[] OUTPUT_SIZE = new int[]{1, 6300, 9};
     private Boolean IS_INT8 = false;
     private final float DETECT_THRESHOLD = 0.25f;
     private final float IOU_THRESHOLD = 0.45f;
     private final float IOU_CLASS_DUPLICATED_THRESHOLD = 0.7f;
-    private final String MODEL_YOLOV5S = "bestn-fp16.tflite";
+    //private final String MODEL_YOLOV5S = "bestn-fp16.tflite";
+    private final String MODEL_YOLOV5S ="320best-fp16.tflite";
 //    private final String MODEL_YOLOV5S = "yolov5s-dynamic.tflite";
     private final String MODEL_YOLOV5N =  "yolov5n-fp16-320.tflite";
     private final String MODEL_YOLOV5M = "yolov5m-fp16-320.tflite";
